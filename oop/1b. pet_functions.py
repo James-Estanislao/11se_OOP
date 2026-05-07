2f30247bebd18a80469ee83d6d4c80ea76b0693c
James-Estanislao <jhaime.re17@gmail.com>
2026-05-07 00:14:25 +0000

    pets update

--- oop/1b. pet_functions.py
+++ oop/1b. pet_functions.py
@@ -31,6 +31,26 @@ print(age)
 #There are many ways to complete these. How will you go about the job?
 #1. Verify this number 1234 4334 1001 0000
 num = '1234 4334 1001 0000'
+if verify_credit_card(num) == True:
+  print('VALID')
+else:
+  print('INVALID')
+
 #2. Ask the user for a credit card number and let them know if it is valid
+
+num = input('please enter Credit Card Number')
+if verify_credit_card(num) == True:
+  print('VALID')
+else:
+  print('INVALID')
+
 #3. If the credit card is valid then reduce balance by $39
+
+num = input('please enter Credit Card Number')
+if verify_credit_card(num) == True:
+  print('VALID')
+  
+else:
+  print('INVALID')
+
 #4. Write and test a function to vaccinate Bonnie 
